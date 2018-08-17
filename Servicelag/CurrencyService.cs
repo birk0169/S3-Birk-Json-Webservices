84da798a5f0f5dc5b8bbd1df10311c6183fa62f0
birk0169 <birk0169@CV-PC-U-24>
2018-08-17 11:56:02 +0200

    Added some more classes for additional Json/Generic class testing

--- Servicelag/CurrencyService.cs
+++ Servicelag/CurrencyService.cs
@@ -33,12 +33,35 @@ namespace Servicelag
             return GetGenericDataFromJson<CurrencyRate>("https://openexchangerates.org/api/latest.json?app_id=2d4606bf1f1949de88d88a2de69b0f78");
         }
 
-        public WeatherData GetWeatherData()
+        ///// <summary>
+        ///// Gets the weather data from the API and returns them in the form of a WeatherData object doesn't work at the moment
+        ///// </summary>
+        ///// <returns>
+        ///// WeatherData object</returns>
+        //public WeatherData GetWeatherData()
+        //{
+        //    return GetGenericDataFromJson<WeatherData>("http://api.openweathermap.org/data/2.5/forecast?q=London&mode=json&units=metric&APPID=098ea01b34673a2948c893da06c6424c;");
+        //}
+
+        /// <summary>
+        /// Gets the nasa data from the API and returns it in the form of a NasaData object
+        /// </summary>
+        /// <returns>
+        /// NasaData object
+        /// </returns>
+        public NasaData GetNasaData()
         {
-            return GetGenericDataFromJson<WeatherData>("http://api.openweathermap.org/data/2.5/forecast?q=London&mode=json&units=metric&APPID=098ea01b34673a2948c893da06c6424c;");
+            return GetGenericDataFromJson<NasaData>("https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo");
         }
 
-        
+        /// <summary>
+        /// Returns the ip adresse of the computer it is called on, from its API
+        /// </summary>
+        /// <returns>MyIpAdresse object</returns>
+        public MyIpAdresse GetMyIpAdresse()
+        {
+            return GetGenericDataFromJson<MyIpAdresse>("http://ip.jsontest.com/");
+        }
         
         /// <summary>
         /// Loads JSON data from a online API and coverts it to a class opject based on input
