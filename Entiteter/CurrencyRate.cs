5963825295e8dba4e0c21960561853958b6b982a
birk0169 <birk0169@CV-PC-U-24>
2018-08-17 09:13:32 +0200

    Added summaries

--- Entiteter/CurrencyRate.cs
+++ Entiteter/CurrencyRate.cs
@@ -6,12 +6,48 @@ using System.Threading.Tasks;
 
 namespace Entiteter
 {
+    /// <summary>
+    /// This class contains currency data in the form of Disclaimers(string), Lincense(string), Timestamp(long), Base(string) and Rates(Dictionary<string decimal>)
+    /// </summary>
     public class CurrencyRate
     {
+        private string _base;
+
+        //Properties
+        /// <summary>
+        /// Holds the disclaimer note from the API
+        /// </summary>
         public string Disclaimer { get; set; }
+        /// <summary>
+        /// Holds the lincense note from the API
+        /// </summary>
         public string License { get; set; }
+        /// <summary>
+        /// Holds the timestamp from the API
+        /// </summary>
         public long Timestamp { get; set; }
-        public string Base { get; set; }
+        /// <summary>
+        /// Holds the information of what base the currency is set on, this must always be USD and if its anything else it will thow an Exception
+        /// </summary>
+        public string Base
+        {
+            get => _base;
+            set
+            {
+                if (value == "USD")
+                {
+                    _base = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Base is not USD");
+                }
+                
+            }
+        }
+        /// <summary>
+        /// Holds the data about the currency rates and their keys based on the currency base
+        /// </summary>
         public Dictionary<string, decimal> Rates { get; set; }
     }
 }
---
+++ Servicelag/CurrencyService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entiteter;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Servicelag
+{
+    public class CurrencyService
+    {
+        /// <summary>
+        /// Gets the currency data from the API and returns them in the form of a CurrencyRate object
+        /// </summary>
+        /// <returns></returns>
+        public CurrencyRate GetCurrencyData()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                string json = webClient.DownloadString(@"https://openexchangerates.org/api/latest.json?app_id=2d4606bf1f1949de88d88a2de69b0f78");
+
+                CurrencyRate result = JsonConvert.DeserializeObject<CurrencyRate>(json);
+                return result;
+            }
+        }
+        
+    }
+}
